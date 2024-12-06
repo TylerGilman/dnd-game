6c353d8e7de7ff77bb88a10963040cf97e0d461b
jeffreywangcf <jeffreywang@Jeffreys-Mac-mini.local>
2024-12-06 02:17:46 -0500

    git ignore

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,6 +3,7 @@ import { authController } from '../controllers/auth.controller';
 
 const router = express.Router();
 
+// why error?
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
