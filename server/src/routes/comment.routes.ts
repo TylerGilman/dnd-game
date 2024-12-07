0630e0ef8dea27e6b3858fa7107215bdcdda71ee
jeffreywangcf <jeffreywangcf@gmail.com>
2024-12-07 01:01:53 -0500

    added ts ignore

--- server/src/routes/comment.routes.ts
+++ server/src/routes/comment.routes.ts
@@ -4,12 +4,13 @@ import { auth } from '../middleware/auth';
 
 const router = express.Router();
 
+//@ts-ignore
 router.post('/:postId/create', auth, commentController.createComment);
-
+//@ts-ignore
 router.get('/:postId', commentController.getCommentsForPost);
 
 router.put('/:commentId', auth, commentController.updateComment);
-
+//@ts-ignore
 router.delete('/:commentId', auth, commentController.deleteComment);
 
 export default router;
--- server/src/routes/post.routes.ts
+++ server/src/routes/post.routes.ts
@@ -3,15 +3,15 @@ import { postController } from '../controllers/post.controller';
 import { auth } from '../middleware/auth';
 
 const router = express.Router();
-
+//@ts-ignore
 router.post('/create', auth, postController.createPost);
-
+//@ts-ignore
 router.get('/:postId', postController.getPost);
 
 router.get('/', postController.getPosts);
 
 router.put('/:postId', auth, postController.updatePost);
-
+//@ts-ignore
 router.delete('/:postId', auth, postController.deletePost);
 
 router.post('/:postId/upvote', auth, postController.upvotePost);
